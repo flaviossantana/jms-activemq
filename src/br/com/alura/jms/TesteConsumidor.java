184f01aec108fee848a7d79363c1060d7487272e
flavio.santana <9090flavio@gmail.com>
2018-06-28 21:52:00 -0300

    Consumindo mensagens com JMS

--- src/br/com/alura/jms/TesteConsumidor.java
+++ src/br/com/alura/jms/TesteConsumidor.java
@@ -1,9 +1,46 @@
 package br.com.alura.jms;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
 public class TesteConsumidor {
-	
-	private void sysout() {
+
+	public static void main(String[] args)  {
+
+		try {
 		
+			InitialContext context = new InitialContext();
+			ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+			
+			Connection connection = factory.createConnection();
+			connection.start();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			
+			Destination destination = (Destination) context.lookup("financeiro");
+			MessageConsumer consumer = session.createConsumer(destination);
+			
+			Message message = consumer.receive();
+			
+			System.out.println("## Mensagem: " + message);
+			
+			//new Scanner(System.in).nextLine();
+			
+			session.close();
+			connection.close();
+			context.close();
+			
+		} catch (Exception e) {
+			System.out.println("DEU ERRO: " + e.getMessage());
+		}
+		
+		
+		
+
 	}
 
 }
